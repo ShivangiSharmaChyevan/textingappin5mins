228239ed8fe2293d9f732e8d0fb67c478fef7a72
Shivangi Sharma <shivangisharma2205@gmail.com>
2017-08-25 12:58:16 +0000

    Update send.py

--- send.py
+++ send.py
@@ -5,6 +5,6 @@ from credentials import acc_sid, auth_token, my_cell, my_twilio
 
 client = Client(acc_sid, auth_token)
 
-my_msg = "chutiya..."
+my_msg = "Hello..."
 
 message = client.messages.create(to=my_cell, from_=my_twilio,body=my_msg)
