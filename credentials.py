9fedf64a53a18b611348bd96d85739b4cb4d82b1
Shivangi Sharma <shivangisharma2205@gmail.com>
2017-08-25 12:55:53 +0000

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+acc_sid='your sid'
+auth_token='your auth_token'
+my_cell='your cell'
+my_twilio='your_twilio'
---
+++ send.py
@@ -0,0 +1,10 @@
+from twilio.rest import Client
+from twilio.rest import TwilioRestClient
+from credentials import acc_sid, auth_token, my_cell, my_twilio
+
+
+client = Client(acc_sid, auth_token)
+
+my_msg = "chutiya..."
+
+message = client.messages.create(to=my_cell, from_=my_twilio,body=my_msg)
